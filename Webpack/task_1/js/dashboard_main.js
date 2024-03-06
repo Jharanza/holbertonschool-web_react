0c649a3a8e4c6e8138044389c2219d067da30dd3
jhony arana <jharanza1986@gmail.com>
2024-03-05 19:26:43 -0500

    feat: create a project with webpack, with a delay using Lodash

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,24 @@
+import $ from 'jquery';
+const _ = require('lodash')
+
+let count = 0
+function updateCounter() {
+    count++;
+    $('#count').text(`${count} clicks on the button`)
+}
+
+$(document).ready(() => {
+    $('body').append(`
+        <p>Holberton Dashboard</p>
+        <p>Dashboard data for the students</p>
+        <button>Click here to get started</button>
+        <p id='count'></p>
+        <p>Copyright - Holberton School</p>
+    `)
+
+    const debouncedUpdateCounter = _.debounce(updateCounter, 500);
+    $('button').click(debouncedUpdateCounter)
+});
+
+
+
