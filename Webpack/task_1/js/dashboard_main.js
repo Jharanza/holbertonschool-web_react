b75bba657e5256311cb2d137856f43a57849dcb5
jhony arana <jharanza1986@gmail.com>
2024-03-06 00:46:30 -0500

    feat: Change the form to append elements

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,24 +1,19 @@
 import $ from 'jquery';
-const _ = require('lodash')
+import _  from 'lodash';
 
 let count = 0
 function updateCounter() {
-    count++;
-    $('#count').text(`${count} clicks on the button`)
+  count++;
+  $('#count').text(`${count} clicks on the button`);
 }
 
 $(document).ready(() => {
-    $('body').append(`
-        <p>Holberton Dashboard</p>
-        <p>Dashboard data for the students</p>
-        <button>Click here to get started</button>
-        <p id='count'></p>
-        <p>Copyright - Holberton School</p>
-    `)
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button>Click here to get started</button>');
+  $('body').append("<p id='count'></p>");
+  $('body').append('<p>Copyright - Holberton School</p>');
 
-    const debouncedUpdateCounter = _.debounce(updateCounter, 500);
-    $('button').click(debouncedUpdateCounter)
+  const debouncedUpdateCounter = _.debounce(updateCounter, 500);
+  $('button').click(debouncedUpdateCounter);
 });
-
-
-
