25e52c2ffb78e1111723ce4ebc1d1c113bf26b87
jhony arana <jharanza1986@gmail.com>
2024-03-05 23:35:26 -0500

    feat: configure webpack and display images

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    entry: './js/dashboard_main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif)/i,
+                use: ['file-loader']
+            }
+        ]
+    }
+}
