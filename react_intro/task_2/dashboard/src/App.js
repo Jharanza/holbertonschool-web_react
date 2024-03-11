d49135bffb6f01c6c5250f024738297049c16d42
jhony arana <jharanza1986@gmail.com>
2024-03-11 13:24:07 -0500

    feat: Add messages in the notifications component

--- react_intro/task_2/dashboard/src/App.js
+++ react_intro/task_2/dashboard/src/App.js
@@ -1,50 +1,48 @@
-import logo from './holberton-logo.jpg';
-import React, { useState } from 'react';
-import { getFooterCopy, getFullYear } from './utils';
+import { useState } from 'react';
+import logo from './HolbertonLogo.jpg';
+import { getFullYear, getFooterCopy } from './utils';
 import './App.css';
 
 function App() {
 
-  const year = getFullYear();
-  const footerCopy = getFooterCopy();
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [ email, setEmail] = useState('');
+  const [ password, setPassword] = useState('');
 
   const handleEmailChange = (e) => setEmail(e.target.value);
+
   const handlePasswordChange = (e) => setPassword(e.target.value);
 
   return (
     <div className="App">
-        <div className='App-header'>
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>School dashboard</h1>
-        </div>
-        <div className='App-body'>
-          <p>Login to access the full dashboard</p>
-          <div className='login'>
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <div className='login'>
           <label htmlFor='email'>Email</label>
           <input 
-            type='email'
-            id='email'
+            type="email"
+            id="email"
             value={ email }
             onChange={ handleEmailChange }
             autoComplete='true'
           ></input>
           <label htmlFor='password'>Password</label>
           <input 
-            type='password'
+            type="password"
             id='password'
             value={ password }
             onChange={ handlePasswordChange }
             autoComplete='true'
           ></input>
           <button>OK</button>
-          </div>
         </div>
-        <div className='App-footer'>
-          <p>Copyright { year } - { footerCopy }</p>
-        </div>
+      </div>
+      <div className="App-footer">
+        <p>Copyright { getFullYear() } - { getFooterCopy() }</p>
+      </div>
     </div>
   );
 }
--- react_intro/task_2/dashboard/src/index.js
+++ react_intro/task_2/dashboard/src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Notifications from './Notifications';
+import { Notifications } from './Notifications';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div className='root-notifications'>
-      <Notifications />
+      < Notifications />
     </div>
     <App />
   </React.StrictMode>
