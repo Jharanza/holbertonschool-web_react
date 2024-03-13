4a0f66214a0d1b3d51a445a24d95f3430350890d
jhony arana <jharanza1986@gmail.com>
2024-03-12 20:40:08 -0500

    feat: update the package.json

--- react_intro/task_3/dashboard/src/utils.test.js
+++ react_intro/task_3/dashboard/src/utils.test.js
@@ -1,4 +1,4 @@
-import { getFooterCopy, getFullYear, getLatestNotification } from "./utils";
+import { getFullYear, getFooterCopy, getLatestNotification } from "./utils";
 
 test('getFullYear returns current year', () => {
     const currentYear = new Date().getFullYear().toString();
@@ -7,7 +7,7 @@ test('getFullYear returns current year', () => {
     expect(fullYear).toBe(currentYear);
 });
 
-test('getFooterCopy returns "Holberton School" if parameter is true', () => {
+test('getFooterCopy returns "Holberton School" if isIndex is true', () => {
     const isIndex = true;
     const msg = 'Holberton School';
     const footerCopy = getFooterCopy(isIndex);
@@ -15,7 +15,7 @@ test('getFooterCopy returns "Holberton School" if parameter is true', () => {
     expect(footerCopy).toBe(msg);
 });
 
-test('getFooterCopy returns "Holberton School main dashboard" if paramenter is false', () => {
+test('getFooterCopy returns "Holberton School main dashboardl" if isIndex is false', () => {
     const isIndex = false;
     const msg = 'Holberton School main dashboard';
     const footerCopy = getFooterCopy(isIndex);
@@ -23,9 +23,9 @@ test('getFooterCopy returns "Holberton School main dashboard" if paramenter is f
     expect(footerCopy).toBe(msg);
 });
 
-test('getLastestNotification returns the mesage <strong>Urgent requirement</strong> - complete by EOD', () => {
+test('getLatestNotification returns "<strong>Urgent requirement</strong> - complete by EOD"', () => {
     const msg = '<strong>Urgent requirement</strong> - complete by EOD';
-    const latestNotification = getLatestNotification();
+    const notification = getLatestNotification();
 
-    expect(latestNotification).toBe(msg);
-});
+    expect(notification).toBe(msg);
+})
