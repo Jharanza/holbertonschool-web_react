51361568009554e3fd38ae89916cfa453aef4d4d
jhony arana <jharanza1986@gmail.com>
2024-03-13 22:22:47 -0500

    feat: push the project to github pages

---
+++ react_intro/task_4/dashboard/src/Notifications.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Notifications.css'
+import close  from './close.png'
+import { getLatestNotification } from './utils';
+
+export const Notifications = () => {
+
+    const clickBotton = () => console.log('Close button has been clicked')
+
+    return (
+        <div className='Notifications'>
+            
+            <button 
+                aria-label='Close'
+                style={{
+                    position: 'absolute',
+                    right: '20px',
+                    top: '20px',
+                    padding: '5px 10px',
+                    height: '20px',
+                    width: '20px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    backgroundColor: 'white',
+                    border: 'none'
+                }}
+                onClick={ clickBotton }
+            ><img src={ close } alt='close'></img></button>
+            <p>Here is the list of notifications</p>
+            <ul>
+                <li data-priority='default'>New course available</li>
+                <li data-priority='urgent'>New resume available</li>
+                <li data-priority='urgent' dangerouslySetInnerHTML={{ __html: getLatestNotification()}}></li>
+            </ul>
+        </div>
+    )
+}
